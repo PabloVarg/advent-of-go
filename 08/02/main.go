a9c1bce4219647102064a9cd3fb79b6be99319a7
Pablo Vargas <49590991+PabloVarg@users.noreply.github.com>
2024-12-12 21:50:27 -0600

    feat(puzzle): Implement day 8 part 2

---
+++ 08/02/main.go
@@ -0,0 +1,70 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/PabloVarg/advent-of-go/input"
+)
+
+func main() {
+	mat := readInput()
+	result := simulate(mat)
+
+	fmt.Println(result)
+}
+
+func simulate(mat [][]rune) int {
+	m, n := len(mat), len(mat[0])
+
+	positions := make(map[rune][][]int)
+	for row := range mat {
+		for col, v := range mat[row] {
+			if v == '.' {
+				continue
+			}
+
+			positions[v] = append(positions[v], []int{row, col})
+		}
+	}
+
+	seen := make([][]bool, len(mat))
+	for row := range seen {
+		seen[row] = make([]bool, len(mat[row]))
+	}
+
+	result := 0
+	for _, pos := range positions {
+		for i := range pos {
+			for j := range pos {
+				if i == j {
+					continue
+				}
+
+				vec := []int{pos[i][0] - pos[j][0], pos[i][1] - pos[j][1]}
+
+				mul := 0
+				r, c := pos[i][0]+vec[0]*mul, pos[i][1]+vec[1]*mul
+				for inBounds(m, n, r, c) {
+					if !seen[r][c] {
+						seen[r][c] = true
+						result++
+					}
+
+					mul++
+					r, c = pos[i][0]+vec[0]*mul, pos[i][1]+vec[1]*mul
+				}
+			}
+		}
+	}
+
+	return result
+}
+
+func inBounds(m, n, r, c int) bool {
+	return r >= 0 && r < m && c >= 0 && c < n
+}
+
+func readInput() [][]rune {
+	return input.ReadRuneMat(os.Stdin)
+}
